fd261a59d069ba872c75d2c4633d64c68968972d
Trevor Thompson <thompsontrevor67@gmail.com>
2025-11-16 18:22:05 -0500

    Initial Commit

---
+++ primallife/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  primallife
+//
+//  Created by Trevor Thompson on 11/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ primallife/primallifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  primallifeApp.swift
+//  primallife
+//
+//  Created by Trevor Thompson on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct primallifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

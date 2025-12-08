7786e8d2261352f7fc6425d5b74859be38e12d68
Trevor Thompson <thompsontrevor67@gmail.com>
2025-12-07 22:50:28 -0500

    OnboardingViewModel

---
+++ primallife/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import Combine
+
+final class OnboardingViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var birthday = Date()
+    @Published var hasSelectedBirthday = false
+    
+    @Published var selectedLanguageIDs: Set<String> = []
+    @Published var selectedCountryID: String?
+    @Published var selectedGender: String?
+    @Published var travelCompanionPreference: String?
+    @Published var meetingStyle: String?
+    @Published var selectedInterests: Set<String> = []
+    @Published var splitExpensesPreference: String?
+    @Published var travelDescription: String?
+    
+    @Published var bio = ""
+    
+    @Published var upcomingDestination = ""
+    @Published var arrivalDate = Date()
+    @Published var hasSelectedArrival = false
+    @Published var departingDate = Date()
+    @Published var hasSelectedDeparting = false
+    
+    @Published var profileImageData: Data?
+    @Published var avatarPath: String?
+}
